e1cd96df43995d92e3a035ebb139bc2360356e73
Matt Kimmel <matt@infinite-jest.net>
2015-05-26 17:41:06 -0400

    Fix redundant event bug

--- github.py
+++ github.py
@@ -3,6 +3,13 @@
 # A simple script to poll Github for interesting events and
 # print them on the Adafruit IoT Printer.
 
+# It turns out that etags are helpful but not sufficient for
+# "bookmarking" github requests. Due to the way Github implements
+# etags, you can get redundant data if some portion of an object
+# you've already seen changes in some way. So we use etags as the
+# "first line of defense" to keep us from getting in rate limiting
+# trouble, and other mechanisms to detect redundant info.
+
 from __future__ import print_function
 import sqlite3
 import github3
@@ -23,6 +30,7 @@ printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 def setup_database(conn):
     c = conn.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS github_etags (request_name text primary key, etag text)')
+    c.execute('CREATE TABLE IF NOT EXISTS github_ids (request_name text primary key, id integer)')
     conn.commit()
     c.close()
 
@@ -60,6 +68,30 @@ def save_etag(conn, request_name, etag):
         print("save_etag encountered exception: %s %s" % (str(type(e)), str(e)))
 
 
+def get_id(conn, request_name):
+    try:
+        id = 0
+        c = conn.cursor()
+        c.execute('SELECT id FROM github_ids WHERE request_name = ?', (request_name,))
+        row = c.fetchone()
+        if row:
+            id = long(row[0])
+        c.close()
+        return id
+    except Exception, e:
+        print("get_id encountered exception: %s %s" % (str(type(e)), str(e)))
+        return 0
+
+def save_id(conn, request_name, id):
+    try:
+        c = conn.cursor()
+        c.execute('INSERT OR REPLACE INTO github_ids (request_name, id) VALUES (?, ?)', (request_name, id))
+        conn.commit()
+        c.close()
+    except Exception, e:
+        print("save_id encountered exception: %s %s" % (str(type(e)), str(e)))
+
+
 def connect_github():
     try:
         return github3.login(github_username, token=github_token)
@@ -94,9 +126,19 @@ def main():
         exit(1)
 
     my_events_etag = get_etag(conn, 'my_events')
+    my_events_last_id = get_id(conn, 'my_events')
+    my_events_highest_id = -1
     my_events = ghclient.me().received_events(etag=my_events_etag)
     for e in my_events:
         event = e.as_dict()
+        # We keep track of event IDs here in case the etag doesn't prevent
+        # us from getting events we've already seen (although ids being
+        # sequential is based on anecdotal evidence).
+        event_id = long(event['id'])
+        if my_events_highest_id == -1 or event_id > my_events_highest_id:
+            my_events_highest_id = event_id
+        if event_id <= my_events_last_id:
+            continue
         if skip_org_events and 'org' in event:
             continue
         if event['type'] == 'WatchEvent':
@@ -108,6 +150,8 @@ def main():
             printer.print(event['actor']['login'] + ' forked repo ' + event['repo']['name'])
             printer.feed(3)
     save_etag(conn, 'my_events', my_events.etag)
+    if my_events_highest_id > my_events_last_id:
+        save_id(conn, 'my_events', my_events_highest_id)
 
     my_followers_etag = get_etag(conn, 'my_followers')
     my_followers = ghclient.me().followers(etag=my_followers_etag)
